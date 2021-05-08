cd934292d34181542063d877cb948be7d694489f
AnthonySar <anthony.sarapata@gmail.com>
2021-05-08 17:20:57 +0200

    First HP & routing

---
+++ farmersShopBo/src/app/services/api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getAuthors() {
+    return this.http.get<any>(environment.apiUrl + 'authors');
+  }
+}
